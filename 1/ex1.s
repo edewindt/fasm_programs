fcfa0b4cae5ec4514770dcdac53e3ed8552a26cf
Elias De Windt <edewindt27@outlook.com>
2023-09-28 03:11:23 -0700

    initial commit

---
+++ 1/ex1.s
@@ -0,0 +1,11 @@
+format elf
+
+section .text
+global _start
+
+_start:
+    mov eax, 1      ; Loads the syscall number for the interrupt
+    mov ebx, 69     ; Loads exit code into ebx register
+    int 0x80        ; This performs a syscall for the 0x80 interrupt
+
+section .data
